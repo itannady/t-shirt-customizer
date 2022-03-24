28775aefb89b920978ebd848a0f3cac65f34c7ad
Isabella Tannady <isabellatannady@gmail.com>
2022-03-24 14:39:32 -0700

    feat(input): add red error for input text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 let text = document.getElementById('userText');
 const out = document.getElementById('tshirt-preview');
 
+//We tried to display the text on the Tee shirt using the following steps;
+// We declared the Id 'userText'using dom and declared the output 'tshirt-preview' using dom
+// To delimit the length of the text, we used an if statment to change the color of the text if it exceeds 20 letters
+// the default is the output equals the value of the text input.
+// We used classlist.add to add is-invalid class to the text variable.
+
 function displayText () {
+if (text.value.length > 20) {
+    document.querySelector('.maxNum').style.color = 'red';
+    text.classList.add('is-invalid');
+}
+else {
     out.innerHTML = text.value;
 }
-
+}
