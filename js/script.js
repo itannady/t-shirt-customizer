b2dcc33f87daa4aff6912bcb64bd4a460b91bf84
unknown <ivan.piceno171@gmail.com>
2022-03-29 17:05:31 -0700

    feat day 2

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ else {
 }
 }
 
+
 // 1. When user selects a different font in the dropdown, change the t-shirt font to that font.
 //     1. Use [onchange](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event) event to detect change in the dropdown and slider.
 
@@ -43,12 +44,68 @@ let green = document.querySelector(".green");
 let red = document.querySelector(".red");
 
 black.addEventListener('click',function onClick(){
-shirtText.style.color= 'black'
-;
+shirtText.style.color= 'black';
 });
 
 blue.addEventListener('click',function onClick(){
-    shirtText.style.color= 'blue'
-    ;
-    });
+    shirtText.style.color= 'blue';
+});
 
+
+red.addEventListener('click',function onClick(){
+    shirtText.style.color= 'red';
+ });
+    
+green.addEventListener('click',function onClick(){
+    shirtText.style.color= 'green';
+ });
+
+//Check that "Text to put on T-shirt" input field is not empty and is not longer than 20 characters. 
+//Show red error message below Add to Cart button and add .is-invalid class around teh text input to display a red border around it
+//Do not update shopping cart if rules are not met
+
+ let subtotal = 25;
+ let tax = 5;
+ let total = 0; 
+
+ document.getElementById('cart_button').onclick= function() {
+      addCart(Number(document.getElementById('subtotal').innerHTML))
+ }
+
+    function addCart() {
+        total = subtotal + tax
+        console.log(total)
+      document.getElementById('total').innerHTML= total
+
+    }
+
+     addCart(total)
+
+// let subtotal = 0;
+// 	let cost = 0;
+// 		//Click = Money
+// 		document.querySelector('#hamster1').onclick= function () {
+// 			cartSubtotal( Number(document.getElementById('ham1').innerHTML))
+
+// 		}
+
+// document.querySelector('#hamster1').onclick= function () {
+//     cartSubtotal( Number(document.getElementById('ham1').innerHTML))
+
+// }
+
+// function cartSubtotal(cost){ 
+//     console.log(cost)
+//     subtotal += cost
+//     document.querySelector('#cost').innerHTML= subtotal
+// }
+// console.log(hamster1)
+
+
+
+   //Get the price of the T-shirt
+//    function addCart() {
+//     price = Number(cart_button.getAttribute('data-price'));
+//      total = total + price;
+//     document.getElementById('subtotal').innerHTML = total.toFixed();
+// }
